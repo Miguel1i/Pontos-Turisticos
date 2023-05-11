da963761e39879ee33a2bef2f2051f31c0f7ca23
Miguel Rego <miguelrego14@gmail.com>
2023-05-11 14:30:59 +0000

    Adição de comentários na classe sistema

---
+++ interface.py
@@ -0,0 +1,10 @@
+from sistema import Sistema
+
+def menu(sistema: Sistema):
+
+    ans = input('Escolha a opção')
+
+    match ans:
+
+       case 1:
+           sistema.
---
+++ sistema.py
@@ -0,0 +1,54 @@
+class Sistema:
+
+    def __init__(self):
+        """
+        Método construtor da classe Sistema
+        """
+        pass
+
+    def adicionar_ponto(self) -> None:
+        """
+        Permite adicionar um novo ponto de interesse turístico.
+        :return: None
+        """
+        pass
+
+    def alterar_ponto(self) -> None:
+        """
+        Permite alterar as categorias e acessibilidade de um ponto de interesse turístico
+        :return: None
+        """
+        pass
+
+    def pesquisar_pontos(self):
+        """
+        Permite pesquisar pontos de interesse turísticos por categorias.
+        :return:
+        """
+
+        pass
+
+    def assinalar_avaliar_ponto(self):
+        """
+        Permite incrementar numa unidade o contador de visitas de um ponto de interesse turístico e atualizar as
+        classificações da experiência da visita nesse ponto.
+        :return:
+        """
+        pass
+
+    def consultar_estatisticas(self):
+        """
+        Mostra os pontos de interesse turísticos, indicando o número de visitantes e a classificação média que foi
+        atribuída.
+        :return:
+        """
+        pass
+
+    def obter_sugestoes(self):
+        """
+        Sugere pontos de interesse turístico para visitar, considerando a proximidade a uma localização geográfica.
+        :return:
+        """
+        pass
+
+
